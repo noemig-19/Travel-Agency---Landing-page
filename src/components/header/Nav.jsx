ea52703dac79871112060a71918bdfddcf12f113
noemi bonilla <noemibonilla95@hotmail.com>
2026-07-26 16:02:19 +0200

    creazione del nav

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,11 +1,26 @@
 
 function Nav() {
 
-  return (
-   <nav>
-    <h2>nav</h2>
-   </nav>
-  )
+    return (
+        <nav className="p-8">
+            <div className="flex justify-around ">
+                <div className="font-bold text-2xl text-emerald-700 cursor-pointer">
+                    Viaggiamo
+                </div>
+                <div className="flex gap-4 text-lg font-bold text-emerald-950 cursor-pointer ">
+                    <li className=" hover:text-emerald-600 transition duration-300">Splora</li>
+                    <li className=" hover:hover:text-emerald-600 transition duration-300">Viaggi</li>
+                    <li className=" hover:hover:text-emerald-600 transition duration-300"> Vedi di piu</li>
+                </div>
+                <div className="text-lg font-bold text-emerald-50 ">
+                    <button className="px-4 py-2 bg-emerald-800 rounded-4xl cursor-pointer hover:bg-emerald-600 transition duration-300 ">
+                        Accedi
+                    </button>
+                </div>
+            </div>
+
+        </nav>
+    )
 }
 
 export default Nav
