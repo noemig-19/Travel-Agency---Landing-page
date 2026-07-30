3bc609c9799d13c0833dea4498542055a5b57bc9
noemi bonilla <noemibonilla95@hotmail.com>
2026-07-30 19:04:44 +0200

    Stayle: card-section tipologye e posizion bg

--- src/components/main/CardSection.jsx
+++ src/components/main/CardSection.jsx
@@ -2,29 +2,32 @@
 function CardSection() {
     return (
         <section className="w-screen flex justify-center gap-8 p-4 my-20">
-            <div className="card-main bg-[url(./assets/car-1.jpg)] bg-cover ">
-                <h1>
+            <div className="card-main bg-[url(./assets/car-1.jpg)] ">
+                <h1 className=" text-white/70 font-bold mt-12">
                     Voli, hotel e auto in un click
                 </h1>
-                <h2>
-                    Organizza il tuo viaggio su misura e risparmia fino al 30% prenotando tutto insieme.
+                <h2 className="text-2xl font-extrabold text-white p-4">
+                    Organizza il tuo viaggio su misura e risparmia fino al <span className="text-4xl font-serif">30%</span> prenotando tutto.
                 </h2>
+                <button className="button-card-section">Scopri di piu</button>
             </div>
-            <div className="card-main bg-[url(./assets/card-2.jpg)] bg-cover"> 
-                <h1>
+            <div className="card-main bg-[url(./assets/card-2.jpg)]"> 
+                <h1 className=" text-white/70 font-bold mt-12">
                     Pacchetti su misura per te
                 </h1>
-                <h2>
-                    Combina volo, hotel e noleggio auto e ottieni fino a €50 di sconto extra.
+                <h2 className="text-2xl font-extrabold text-white p-4">
+                    Combina volo, hotel e noleggio auto e ottieni fino a <span className="text-4xl font-serif">50$</span>  di sconto extra.
                 </h2>
+                <button className="button-card-section">Scopri di piu</button>
             </div>
-            <div className="card-main bg-[url(./assets/spiaggia.jpg)] bg-cover"> 
-                <h1>
+            <div className="card-main bg-[url(./assets/spiaggia.jpg)]"> 
+                <h1 className=" text-white/70 font-bold mt-12">
                     Viaggia senza pensieri
                 </h1>
-                <h2>
+                <h2 className="text-2xl font-extrabold text-white p-4">
                     Prenota volo, alloggio e trasporto in un'unica piattaforma, con le migliori tariffe garantite.
                 </h2>
+                <button className="button-card-section">Scopri di piu</button>
             </div>
         </section>
     )
